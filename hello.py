299d33a904b7c82b4232c1620f447e9ef869b999
Xperiment626 <inaki.manosalvas@gmail.com>
2022-08-04 15:16:11 -0500

    Experimental commit

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print('hello traines!')
+backend_track_names = ['Sebastian', 'Gabriela', 'Karen', 'Daniel', 'Iñaki']
+
+print('Backend Track')
+[print(f'Hi to {name}!') for name in backend_track_names]
